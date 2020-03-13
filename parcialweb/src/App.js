d73269ab740c5fa00ea292206d74d23c0328d410
luis04 <oviedotalaigua@gmail.com>
2020-03-13 15:06:00 -0500

    Parcial

--- parcialweb/src/App.js
+++ parcialweb/src/App.js
@@ -1,25 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
+import "./reset.css"
+import ima from './Imagenes/ima.jpg';
+import fuego from './Imagenes/fuego.png';
+import miguel from './Imagenes/miguel.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="cuerpo">
+        <div className="imag_principal">
+          <div>
+            <img
+              src={ima}
+              height="360px"
+              width="720px"
+              alt="imagen principal"
+            />
+          </div>
+        </div>
+        <div className="cuadro">
+          <div className="cuadro_2">
+            <div className="c_text1">
+              <p className="text1">
+                ¡YA LLEGA EL 2° ANIVERSARIO! <br />
+                ¡CON UNA NUEVA VERSIÓN!
+              </p>
+            </div>
+
+            <div className="c_texto2">
+              <div className="c_texto2_1">
+                <p className="text2">
+                  ¡NUEVO MODO 4 VS 4, NUEVO PERSONAJE, NUEVA <br />
+                  MASCOTA, NUEVA TEM PODEROSA Y MUCHAS MÁS SORPRESAS!
+                </p>
+              </div>
+
+              <div className="c_texto2_2">
+                <p className="text3">
+                  ¡PARA CELEBRAR EN 2° ANIVERSARIO DE FREE FIRE, NOS <br />
+                  NOS GUSTARÍA COMPARTIRTE NUESTROS 4 TEMAS PRINCIPALES <br />
+                  DENTRO DE LA ACTUALIZACIÓN DEL PARCHE DE ESTA SEMANA!
+                </p>
+              </div>
+
+              <div className="fuego">
+                <div>
+                  <img src={fuego} height="80px" width="540px" alt="fuego" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="miguel">
+            <div>
+              <img src={miguel} height="360px" width="190px" alt="Personaje" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
 
